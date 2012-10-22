00ca847f9294e737eabc96d4902ad862244e6937
Jacob Groundwater <groundwater@gmail.com>
2012-10-21 22:05:42 -0700

    Add Demo

---
+++ demo/server.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+var env = process.env;
+
+console.log("Starting Server");
+console.log("Listening to Port", env.PORT);
+console.log("Listening to Address", env.BIND);
+console.log("Conncting to MySQL '%s' as '%s' with Password '%s'",
+	env.MYSQL_HOST,
+	env.MYSQL_USER,
+	env.MYSQL_PASS);
+
+console.log("Server Active for %d Second",process.argv[2]/1000);
+setTimeout(function(){
+    console.log("Server Shutting Down")
+},parseInt(process.argv[2]));
