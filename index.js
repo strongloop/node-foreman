9d6d0dead9d7c6d7d36004120f70c30dd34c6402
Jacob Groundwater <groundwater@gmail.com>
2012-11-11 21:16:33 -0800

    Refactor Proxy

--- index.js
+++ index.js
@@ -193,63 +193,7 @@ function calculatePadding(reqs){
     return padding + 10;
 }
 
-function startProxies(reqs,proc,command){
-	
-	if(command.proxy){
-		
-		var localhost = 'localhost';
-		var i=0;
-		
-		var ports = command.proxy.split(',');
-		for(key in reqs){(function(key){
-			
-			var j = i++;
-			
-			var port = ports[j];
-			
-			if(port<1024 && process.getuid()!=0)
-				return cons.Error('Cannot Bind to Privileged Port %s Without Permission - Try \'sudo\'',port);
-			
-			if(!port) return cons.Warn('No Downstream Port Defined for \'%s\' Proxy',key);
-			if(!(key in proc)) return cons.Warn('Proxy Not Started for Undefined Key \'%s\'',key);
-			
-			var upstream_size = reqs[key];
-			var upstream_port = parseInt(program.port) + j*100;
-			
-			var proxy = prog.fork(__dirname + '/proxy.js',[],{
-				env: {
-					HOST: localhost,
-					PORT: port,
-					UPSTREAM_HOST: localhost,
-					UPSTREAM_PORT: upstream_port,
-					UPSTREAM_SIZE: upstream_size,
-					SUDO_USER: process.env.SUDO_USER
-				}
-			});
-			
-			var port_targets;
-			if(upstream_size==1){
-				port_targets = util.format('%d',   upstream_port);
-			}else{
-				port_targets = util.format('(%d-%d)',upstream_port, upstream_port+upstream_size-1)
-			}
-			
-			cons.Alert('Starting Proxy Server %s -> %s', port, port_targets);
-			
-			emitter.once('killall',function(){
-				cons.Error('Killing Proxy Server on Port %s',port);
-				proxy.kill();
-			})
-		
-			proxy.on('exit',function(){
-				emitter.emit('killall');
-			})
-			
-		})(key)}
-		
-	}
-	
-}
+var startProxies = require('./lib/proxy').startProxies
 
 function startForward(port){
 	var proc = prog.fork(__dirname + '/forward.js',[],{
@@ -302,7 +246,7 @@ program
     
 	if(command.forward) startForward(command.forward);
 	
-	startProxies(reqs,proc,command);
+	startProxies(reqs,proc,command,emitter,program.port);
 	
 	if(process.getuid()==0) process.setuid(process.env.SUDO_USER);
 	
