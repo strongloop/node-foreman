bc026a79f80945f1827c68b049ba3223bb8add13
Jacob Groundwater <groundwater@gmail.com>
2012-11-11 20:40:45 -0800

    Factor Out Console

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ var fs      = require('fs');
 var mu      = require('mu2');
 var events  = require('events');
 
+var lib     = require('./lib')
+
 mu.root = __dirname + '/upstart'
 
 program.version('0.0.8');
@@ -36,78 +38,7 @@ var colors = [
 	function(x){return x.grey}
 ];
 
-var Console = function(){
-	
-	this.fmt = function fmt(){
-	    return util.format.apply(null,arguments);
-	}
-
-	this.pad = function pad(string,n){
-	    var l = string.length;
-	    var d = n - l;
-	    var o = string;
-	    for(i=l;i<n;i++){
-	        o += " "
-	    }
-	    return o + " | ";
-	}
-
-
-	// Process Specific Loggers //
-
-	this.trim = function trim(line,n){
-		var end = '';
-		if(line.length > n){
-			end = '…'
-		}
-		return line.substr(0,n) + end;
-	}
-
-	this.info = function info(key,proc,string){
-	    var stamp = (new Date().toLocaleTimeString()) + " " + key;
-	    console.log(proc.color(this.pad(stamp,padding)),string.white);
-	}
-	
-	this.Info = function info(key,proc,string){
-	    var stamp = (new Date().toLocaleTimeString()) + " " + key;
-	    console.log(proc.color(this.pad(stamp,padding)),string.white.bold);
-	}
-
-	this.log = function log(key,proc,string){
-		var self = this;
-	    string.split(/\n/).forEach(function(line){
-
-	        if (line.trim().length==0) return;
-
-	        var stamp = (new Date().toLocaleTimeString()) + " " + key;
-		
-			if(command.trim>0){
-				line = self.trim(line,command.trim);
-			}else if(command.trim==0){
-				line = self.trim(line,process.stdout.columns - padding - 5);
-			}
-		
-	        console.log(proc.color(self.pad(stamp,padding)),line);
-	    });
-	}
-
-	// Foreman Loggers //
-
-	this.Alert = function Alert(){
-	    console.log( '[OKAY] '.green + cons.fmt.apply(null,arguments).green );
-	}
-	
-	this.Warn = function Warn(prefix){
-	    console.warn('[WARN] '.yellow + cons.fmt.apply(null,arguments).yellow );
-	}
-
-	this.Error = function Error(){
-	    console.error( '[FAIL] '.bold.red + cons.fmt.apply(null,arguments).bold.red );
-	}
-
-}
-
-var cons = new Console();
+var cons = new lib.Console(padding);
 
 // Foreman Event Bus/Emitter //
 
