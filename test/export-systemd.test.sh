a0e547a51fdc49612c246b97117d5115a1b69388
Ryan Graham <ryan@strongloop.com>
2014-03-27 16:20:18 -0700

    test: use strict PATH for tests
    Should make tests pass on other machines

--- test/export-systemd.test.sh
+++ test/export-systemd.test.sh
@@ -5,7 +5,7 @@ NF="node ../nf.js"
 mkdir -p sandbox
 rm -rf sandbox/*
 
-$NF export --app systemd-test --out sandbox --type systemd \
-  --env fixtures/env.env --procfile fixtures/Procfile
+PATH=$(dirname $(which node)) $NF export --app systemd-test --out sandbox \
+  --type systemd --env fixtures/env.env --procfile fixtures/Procfile
 
 diff -r -u fixtures/systemd sandbox 1>&2 || exit $?
--- test/export-upstart.test.sh
+++ test/export-upstart.test.sh
@@ -5,7 +5,7 @@ NF="node ../nf.js"
 mkdir -p sandbox
 rm -rf sandbox/*
 
-$NF export --app upstart-test --out sandbox --type upstart \
-  --env fixtures/env.env --procfile fixtures/Procfile
+PATH=$(dirname $(which node)) $NF export --app upstart-test --out sandbox \
+  --type upstart --env fixtures/env.env --procfile fixtures/Procfile
 
 diff -r -u fixtures/upstart sandbox 1>&2 || exit $?
