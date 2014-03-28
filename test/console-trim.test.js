f671bfac474b673d0ef0b20f07666ae85067066c
Ryan Graham <ryan@strongloop.com>
2014-03-27 21:56:52 -0700

    Ignore ANSI escapes when doing line trimming
    Strip all ANSI escape sequences when determining line length.
     * If the result is long enough to be trimmed, use a trimmed version
       of the stripped line.
     * If the result does not need tirmming, use the line as-is.
    
    Closes #31

---
+++ test/console-trim.test.js
@@ -0,0 +1,20 @@
+// Needed before requiring colors, otherwise its colorizers are no-ops.
+process.stdout.isTTY = true;
+
+var assert = require('chai').assert
+  , util = require('util')
+  , Console = require('../lib/console').Console
+  , colors = require('../lib/colors')
+
+var red = colors.red('red')
+var blue = colors.blue('blue')
+
+var long = 'Roses are red, Violets are blue, this string is long, and should be trimmed, too!'
+var colorLong = long.replace('red', red).replace('blue', blue);
+
+assert.lengthOf(long, 81)
+assert.lengthOf(colorLong, 99)
+assert.equal(Console.trim(colorLong, 50), Console.trim(long, 50))
+
+assert.equal(Console.trim(colorLong, long.length), colorLong,
+             'trim() should leave colors intact if no trimming is performed')
