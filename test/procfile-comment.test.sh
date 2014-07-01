13bafdc28f5d4570dcfabbec6dead347279721ca
Bret Little <blittle@simplifile.com>
2014-07-01 15:30:37 -0600

    Fix the tests

--- test/procfile-comment.test.sh
+++ test/procfile-comment.test.sh
@@ -6,9 +6,10 @@ SED=`which gsed || which sed`
 rm -rf sandbox
 mkdir -p sandbox
 
+echo "PATH=something-totally-bogus" > sandbox/.env
 printf "#norun: touch should-not-exist.js\nrun: touch should-exist.js\n# norun: touch should-not-exist-2.js" > sandbox/Procfile
 
-node ../nf.js export --out sandbox --procfile sandbox/Procfile
+node ../nf.js export --out sandbox --env sandbox/.env --type upstart-single --procfile sandbox/Procfile
 
 $SED -i -e "s%$(pwd)%TEST_DIR%g" \
         -e "s%$(dirname $(which node))%TEST_PATH%g" sandbox/*
