8754833b3a80bc484834b4216f2accc97b55e0ee
Jeff Dickey <dickeyxxx@gmail.com>
2016-04-13 13:32:26 -0400

    fix jshint issues

--- test/envs-empty.test.js
+++ test/envs-empty.test.js
@@ -1,12 +1,12 @@
-var assert = require('assert')
-  , envs   = require('../lib/envs')
+var assert = require('assert');
+var envs   = require('../lib/envs');
 
-var emptyEnvs = ['', '\n', ' \n \n \n']
+var emptyEnvs = ['', '\n', ' \n \n \n'];
 
 emptyEnvs.forEach(function (env) {
-  var loaded = envs.keyValue(env)
+  var loaded = envs.keyValue(env);
   // {} == {}
-  assert.equal(Object.keys(loaded).length, 0)
-})
+  assert.equal(Object.keys(loaded).length, 0);
+});
 
-assert.equal(envs.dumpEnv({}), '\n')
+assert.equal(envs.dumpEnv({}), '\n');
--- test/envs-quoted.test.js
+++ test/envs-quoted.test.js
@@ -1,13 +1,13 @@
-var assert = require('assert')
-  , envs   = require('../lib/envs')
+var assert = require('assert');
+var envs   = require('../lib/envs');
 
 var parsedHash = envs.keyValue(
   '#commented heading. \n' +
   'key = "quoted#hash" \n' +
   'key2 = stripped#comment \n' +
   'key3 = base64=== \n'
-)
+);
 
-assert.equal(parsedHash['key'], 'quoted#hash')
-assert.equal(parsedHash['key2'], 'stripped')
-assert.equal(parsedHash['key3'], 'base64===')
+assert.equal(parsedHash.key, 'quoted#hash');
+assert.equal(parsedHash.key2, 'stripped');
+assert.equal(parsedHash.key3, 'base64===');
