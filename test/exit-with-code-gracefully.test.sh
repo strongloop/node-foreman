bcd3b2333cee2b5db2a34f1894a7c811a621c1f5
Kaspar Schiess <kaspar@pryv.com>
2017-12-21 11:59:13 +0100

    Fix error "Unknown signal: null"
    Also fix the problem of exit code cannot be the signal.
    
    Also handle EPERM when killing processes. EPERM is thrown if the child
    process has managed to acquire more privileges than the parent has.
    This avoids exiting before killing all other processes - nothing can
    be done about the privileged process.

---
+++ test/exit-with-code-gracefully.test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NF="node ../nf.js"
+
+rm -rf sandbox
+mkdir -p sandbox
+
+# We'll exit with a code that cannot be used as a signal. This exposes a
+# potential bug in termination handling.
+printf "exitwithcode: sleep 0.1; exit 132" > sandbox/Procfile
+node ../nf.js --procfile sandbox/Procfile start >sandbox/signals.txt 2>&1 && exit 0
+
+exit 1
+
